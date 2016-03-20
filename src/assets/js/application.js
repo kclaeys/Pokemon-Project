aaf5c3d51366db1c879a9329d6579fdc0b79a733
kclaeys <claeys.k@husky.neu.edu>
2016-03-20 15:04:41 -0400

    Bump

--- src/assets/js/application.js
+++ src/assets/js/application.js
@@ -1,16 +1,19 @@
-GameApp = Backbone.Marionette.Application.extend({
+var Game;
+
+Game = (function() {
+  var GameApp;
+  GameApp = Backbone.Marionette.Application.extend({
     "class": "GameApp",
-    initialize: function (options) {
-
-    },
+    initialize: function(options) {},
     regions: {
-        "menuRegion": "#menu-region",
-        "screenWrapperRegion": "#screen-wrapper-region"
+      "menuRegion": "#menu-region",
+      "screenWrapperRegion": "#screen-wrapper-region"
     },
-    onStart: function () {
-        console.log("GameApp has started!");
-        Game.Menu.Functions.initializeLayout();
-    }
-});
-
-var Game = new GameApp();
+    onStart: function() {
+      console.log("GameApp has started!");
+      Game.Menu.Functions.initializeLayout();
+    },
+    "lightsAreOn": true
+  });
+  return new GameApp();
+})();
